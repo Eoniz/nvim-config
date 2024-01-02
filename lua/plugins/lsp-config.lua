6bbfa99a0cb53853846ac0dff3b5ab67b573ea89
Nathan Artisien <nathan.artisien@momagroup.com>
2024-01-02 06:10:13 +0100

    add: first push
    adding all the files

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,43 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "tsserver", "volar" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local on_attach = function (client, bufnr)
+        local opts = { buffer= bufnr, remap = false }
+
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+        vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "<leader>vref", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "<leader>vrename", function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+      end
+
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({on_attach = on_attach})
+      lspconfig.tsserver.setup({on_attach = on_attach})
+      lspconfig.volar.setup({
+        on_attach = on_attach,
+        filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
+      })
+    end,
+  },
+}
---
+++ lua/vim-options.lua
@@ -0,0 +1,26 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.wrap = false
+
+vim.opt.colorcolumn = "80"
+
+vim.cmd("set number")
+vim.cmd("set expandtab") 
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " "
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("n", "J", "mzJ`z")
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
